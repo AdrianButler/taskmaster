7ab473e155e7ec618d383256d0f9cb57773a2a40
Adrian Butler Jr <butlerjr123@gmail.com>
2022-12-01 15:43:45 -0600

    setup project

---
+++ app/src/main/java/com/adrianbutler/taskmaster/OrderForm.java
@@ -0,0 +1,14 @@
+package com.adrianbutler.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_form);
+    }
+}

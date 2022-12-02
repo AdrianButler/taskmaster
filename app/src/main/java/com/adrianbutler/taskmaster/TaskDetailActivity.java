3335b717422d87036437a774eb09d3382f405835
Adrian Butler Jr <butlerjr123@gmail.com>
2022-12-02 11:47:21 -0600

    added task-detail page

---
+++ app/src/main/java/com/adrianbutler/taskmaster/TaskDetailActivity.java
@@ -0,0 +1,14 @@
+package com.adrianbutler.taskmaster;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaskDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+    }
+}

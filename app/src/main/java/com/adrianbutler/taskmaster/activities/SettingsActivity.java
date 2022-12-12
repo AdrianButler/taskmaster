6159ff814b032594a3430aff95733eadb05e9e3b
Adrian Butler Jr <butlerjr123@gmail.com>
2022-12-12 14:41:32 -0600

    Added team selection to settings page and now only shows you tasks from your team.

--- app/src/main/java/com/adrianbutler/taskmaster/activities/SettingsActivity.java
+++ app/src/main/java/com/adrianbutler/taskmaster/activities/SettingsActivity.java
@@ -3,46 +3,96 @@ package com.adrianbutler.taskmaster.activities;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.adrianbutler.taskmaster.R;
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Team;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    SharedPreferences preferences;
-    public static final String USERNAME_TAG = "username";
+public class SettingsActivity extends AppCompatActivity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        assignSaveButtonHandler();
-    }
+	SharedPreferences preferences;
+	public static final String USERNAME_TAG = "username";
+	public static final String TEAM_TAG = "team";
 
-    private void assignSaveButtonHandler()
-    {
-        Button saveButton = findViewById(R.id.SettingsSaveButton);
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+		preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		setupTeamSpinner();
+		assignSaveButtonHandler();
+	}
 
-        saveButton.setOnClickListener(view -> // save username and display a toast
-        {
-            EditText usernameText = findViewById(R.id.SettingsUsernameEditText);
-            saveUsername(usernameText.getText().toString());
+	private void assignSaveButtonHandler()
+	{
+		Button saveButton = findViewById(R.id.SettingsSaveButton);
 
-            Toast.makeText(this, "Username Saved!", Toast.LENGTH_SHORT).show();
-        });
+		saveButton.setOnClickListener(view -> // save username and display a toast
+		{
+			EditText usernameText = findViewById(R.id.SettingsUsernameEditText);
+			saveUsername(usernameText.getText().toString());
 
-    }
+			Spinner teamDropdown = findViewById(R.id.SettingsActivitySelectTeamDropdown);
+			saveTeam(teamDropdown.getSelectedItem().toString());
 
-    private void saveUsername(String username)
-    {
-        SharedPreferences.Editor preferenceEditor = preferences.edit();
+			Toast.makeText(this, "Username Saved!", Toast.LENGTH_SHORT).show();
+		});
 
-        preferenceEditor.putString(USERNAME_TAG, username);
-        preferenceEditor.apply();
-    }
+	}
+
+	private void saveUsername(String username)
+	{
+		SharedPreferences.Editor preferenceEditor = preferences.edit();
+
+		preferenceEditor.putString(USERNAME_TAG, username);
+		preferenceEditor.apply();
+	}
+
+	private void saveTeam(String teamName)
+	{
+		SharedPreferences.Editor preferenceEditor = preferences.edit();
+
+		preferenceEditor.putString(TEAM_TAG, teamName);
+		preferenceEditor.apply();
+	}
+
+	private void setupTeamSpinner()
+	{
+		Amplify.API.query(ModelQuery.list(Team.class),
+				success ->
+				{
+					Log.i("Settings", "Successfully queried team names");
+					Spinner selectTeamDropdown = findViewById(R.id.SettingsActivitySelectTeamDropdown);
+					ArrayList<String> teamNames = new ArrayList<>();
+					for (Team team : success.getData())
+					{
+						teamNames.add(team.getName());
+					}
+					runOnUiThread(() ->
+					{
+						selectTeamDropdown.setAdapter(new ArrayAdapter<>(
+										this,
+										android.R.layout.simple_spinner_item,
+										teamNames
+								)
+						);
+					});
+
+				},
+				failure -> Log.e("Settings", "Failed to query team names")
+		);
+	}
 }
